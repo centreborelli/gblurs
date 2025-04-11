31f432b7a6e349094363bacf825b28c7ef497560
Enric Meinhardt-Llopis <enric.meinhardt@ens-paris-saclay.fr>
2025-04-11 11:35:57 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+import setuptools
+
+setuptools.setup(
+	name = "gblurs",
+	version = "2",
+	description = "All Gaussian Blurs",
+	classifiers = [
+		"Operating System :: OS Independent",
+		"Topic :: Scientific/Engineering :: Image Processing",
+		"Topic :: Scientific/Engineering :: Mathematics"
+		],
+	author = "Enric Meinhardt-Llopis",
+	author_email = "enric.meinhardt@fastmail.com",
+	py_modules = ["gblurs"]
+)
